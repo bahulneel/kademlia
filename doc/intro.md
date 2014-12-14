2716e7605ec6d1c698dc455fbe83cef3d4fea1ac
Neel Upadhyaya <bahulneel@gmail.com>
2014-12-14 00:39:30 +0000

    New project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kademlia
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject kademlia "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [pandect "0.4.1"]])
